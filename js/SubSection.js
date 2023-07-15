2ca194cf38b65f9ee700fac15dec6b43fb2d52e1
Cesar-CARV <cesaruizvera@gmail.com>
2023-07-15 13:09:31 -0600

    Se agrego la funcionalidad de añadir comentarios

--- js/SubSection.js
+++ js/SubSection.js
@@ -1,6 +1,9 @@
 class SubSection {
     constructor() {
         this.finished = false;
+        this.editComment = false;
+        this.commentSelected = null;
+        this.viewImage = false;
         this.comments = [];
         this.images = [];
         this.score = 0;
@@ -13,16 +16,86 @@ class SubSection {
         return this.section;
     }
 
-    addEvents = () => {
-        const buttonClose = this.section.querySelector('.sub-section__finish-button');
-        buttonClose.addEventListener('click', e => {
-            this.finished = !this.finished;
-            this.render();
-        });
+    generateID = () => {
+        const random = Math.random().toString(36).substr(2);
+        const date = Date.now().toString(36);
+
+        return random + date;
     }
 
     handleClickFinishButton = (e) => {
-        this.finished = true;
+        this.finished = !this.finished;
+        this.render();
+    }
+
+    handleClickAddCommentButton = (e) => {
+        const newId = this.generateID();
+        this.comments.unshift({ id: newId, comment: "Empty comment"});
+        this.render();
+    }
+
+    handleClickDeleteCommentButton = (e) => {
+        const target = e.target.tagName === "I" ? e.target.parentElement.parentElement : e.target.parentElement 
+        const id = target.id;
+        this.comments = this.comments.filter((comment) => comment.id !== id);
+        this.editComment = false;
+        this.render();
+    }
+
+    handleClickEditCommentButton = (e) => {
+        const target = e.target.tagName === "I" ? e.target.parentElement.parentElement.parentElement : e.target.parentElement.parentElement;
+        const id = target.id;
+        this.commentSelected = this.comments.filter((comment) => comment.id === id)[0];
+        this.editComment = true;
+        this.render();
+    }
+
+    handleClickCloseModalEditButton = (e) => {
+        const modalEdit = this.section.querySelector('.modal-edit');
+        this.editComment = false;
+        modalEdit.close();
+    }
+
+    handleClickModalEditForm = (e) => {
+        e.preventDefault();
+        const modalEdit = this.section.querySelector('.modal-edit');
+        const textarea = modalEdit.querySelector('.modal-edit__textarea');
+
+        this.comments = this.comments.map((comment) => {
+            if (comment.id !== this.commentSelected.id) return comment;
+
+            comment.comment = textarea.value;
+            return comment;
+        });
+
+        this.editComment = false;
+        this.render();
+    }
+
+    addEvents = () => {
+        const finishButton = this.section.querySelector('.sub-section__finish-button');
+        finishButton.addEventListener('click', this.handleClickFinishButton);
+
+        const addCommentButton = this.section.querySelector('.sub-section__add-comment-button');
+        addCommentButton.addEventListener('click', this.handleClickAddCommentButton);
+
+        const deleteCommentButton = this.section.querySelectorAll('.delete-comment-button');
+        deleteCommentButton.forEach(button => {
+            button.addEventListener('click', this.handleClickDeleteCommentButton);
+        });
+
+        const editCommentButton = this.section.querySelectorAll('.edit-comment-button');
+        editCommentButton.forEach(button => {
+            button.addEventListener('click', this.handleClickEditCommentButton);
+        });
+
+        const closeModalEditButton = this.section.querySelector('.modal-edit__close-button');
+        closeModalEditButton.addEventListener('click', this.handleClickCloseModalEditButton);
+
+        const modalEditForm = this.section.querySelector('.modal-edit__form');
+        modalEditForm.addEventListener('submit', this.handleClickModalEditForm);
+
+
     }
 
     render = () => {
@@ -30,14 +103,16 @@ class SubSection {
         <!-- Header -->
         <header>
             <div>
-                ${this.finished ?
+                <div>
+                    ${this.finished ?
                 `<i class='bx bxs-check-circle'></i>`
                 :
                 `<i class='bx bx-loader-circle'></i>`
             }
-            </div>
-            <div>
-                <h5>Name of section</h5>
+                </div>
+                <div>
+                    <h5>Name of section</h5>
+                </div>
             </div>
             <div>
                 <button class="sub-section__finish-button">
@@ -60,20 +135,34 @@ class SubSection {
         </div>
         <!-- Buttons -->
         <div>
+            <button class="sub-section__add-comment-button">Add Comment</button>
             <button>Add Image</button>
-            <button>Add Commnet</button>
         </div>
         <!-- Comments section -->
         <div>
             <!-- title -->
             <div><i class='bx bxs-comment-dots'></i><span>Comments</span></div>
             <!-- Comments -->
-            <ul>
+            <ul class="sub-section__comments">
                 <!-- Comment -->
-                <li>
-                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, modi!</span>
-                    <i class='bx bx-edit'></i>
-                </li>
+                ${this.comments.map((comment) => {
+                return `
+                        <li>
+                            <div class="comment" id=${comment.id}>
+                                <div>
+                                    <button class="edit-comment-button">
+                                        <i class='bx bx-edit'></i>
+                                    </button>
+                                    <span>${comment.comment}</span>
+                                </div>
+                                <button class="delete-comment-button">
+                                    <i class='bx bxs-x-circle'></i>
+                                </button>
+                            </div>
+                        </li>
+                        `
+            }).join('')
+            }
             </ul>
         </div>
 
@@ -91,7 +180,7 @@ class SubSection {
         </div>
 
         <!-- View image modal -->
-        <dialog>
+        <dialog ${this.viewImage ? "open='true'" : ""}>
             <section>
                 <header>
                     <button>
@@ -105,20 +194,19 @@ class SubSection {
         </dialog>
 
         <!-- Edit commnet modal -->
-        <dialog>
+        <dialog ${this.editComment ? "open='true'" : ""} class="modal-edit">
             <section>
                 <header>
-                    <button>
+                    <button class="modal-edit__close-button">
                         <i class='bx bxs-x-circle'></i>
                     </button>
                 </header>
-                <div>
-                    <textarea>
-                    </textarea>
-                    <button>
+                <form class="modal-edit__form">
+                    <textarea class="modal-edit__textarea">${this.commentSelected ? this.commentSelected.comment : ""}</textarea>
+                    <button type="submit">
                         Save
                     </button>
-                </div>
+                </form>
             </section>
         </dialog>
         `
