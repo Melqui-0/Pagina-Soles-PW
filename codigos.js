045052898c7d8da6d4ab3ac3cd9776436468c1cc
Melqui Ramirez <melqui_azarel@hotmail.com>
2023-07-16 15:02:23 -0600

    Se trabajo en el header

--- codigos.js
+++ codigos.js
@@ -46,3 +46,30 @@ function capturarfoto () {
 
 //Agregar el evento click al boton de captura
 capturebutton.addEventListener("click", capturarfoto);
+
+//Para los supervisores
+let seleccionar = document.querySelector('select');
+let parrafo = document.querySelector('a');
+let turnos = document.querySelector('input[name="turno"].checked').value;
+
+seleccionar.addEventListener('change', establecersuper);
+
+function establecersuper(){
+    let eleccion = seleccionar.value;
+    let turno = turnos.value;
+
+    if (eleccion === 'MES TEST') {
+     parrafo.textContent = 'Rafael granados';
+    } else if (turno === 2) {
+     parrafo.textContent = 'Ganaste';
+    } 
+    else {
+     parrafo.textContent = ' ';
+    }
+}
+
+document.getElementById('submit').onclick = function() {
+    var radios = document.getElementsByName('turno');
+    var selected = Array.from(radios).find(radio => radio.checked);
+    alert (selected.value)
+}
